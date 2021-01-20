eed34b72bdcbc1649851302b5cfd24b0450a8294
Cryptavy <cryptavy@gmail.com>
2021-01-20 20:57:25 +0800

    added table for the task holder

--- ToDoList.php
+++ ToDoList.php
@@ -11,5 +11,23 @@
         <input type="text" name="task" class="task_input">
         <button type="submit" class="task_btn" name="submit">Add Task</button>
     </form>
+    <table>
+        <thead>
+            <tr>
+                <th>N</th>
+                <th>Task</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                 <td>1</td>
+                <td class="task">This is the Task Holder</td>
+                 <td class="delete">
+                 <a href="#">x</a>
+                </td>
+            </tr>
+        </tbody>
+    </table>
 </body>
 </html>
