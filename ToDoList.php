4aa69208c8d64d2938adfad823cd3e79fa6e23ce
Cryptavy <cryptavy@gmail.com>
2021-01-20 20:26:54 +0800

    added a design for the form

--- ToDoList.php
+++ ToDoList.php
@@ -1,10 +1,15 @@
 <html>
 <head>
         <title> Objectives for Today </title>
+        <link rel="stylesheet" type="text/css" href="styleSheet.css">
 </head>
 <body>
     <div class="heading">
         <h2> Todo List application using PHP and MySQL</h2>
     </div>
+    <form method="POST" action="ToDoList.php">
+        <input type="text" name="task" class="task_input">
+        <button type="submit" class="task_btn" name="submit">Add Task</button>
+    </form>
 </body>
 </html>
