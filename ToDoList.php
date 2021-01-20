9b39855a0c9e051dffce94ac0d1e7df69b1e8aac
Cryptavy <cryptavy@gmail.com>
2021-01-20 19:21:15 +0800

    added a design for the heading

---
+++ ToDoList.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+        <title> Objectives for Today </title>
+</head>
+<body>
+    <div class="heading">
+        <h2> Todo List application using PHP and MySQL</h2>
+    </div>
+</body>
+</html>
